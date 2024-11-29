1641df00c00f8a324ed5ebbe9f2d912521a217cd
Евсеев Егор <Tyuiu.EvseevEI@outlook.com>
2024-11-29 23:11:48 +0500

    4

--- Tyuiu.EvseevEI.Sprint3.Task3.V27.Lib/DataService.cs
+++ Tyuiu.EvseevEI.Sprint3.Task3.V27.Lib/DataService.cs
@@ -5,7 +5,19 @@ namespace Tyuiu.EvseevEI.Sprint3.Task3.V27.Lib
     {
         public int ConvertStringToInt(string value)
         {
-            
+            string input = "!bt, g567kid f!";
+            string result = "";
+
+            foreach (char c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    result += c;
+                }
+            }
+            int number = int.Parse(result);
+            Console.WriteLine(number);
+
         }
     }
 }
